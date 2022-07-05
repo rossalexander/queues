2e6eb2268c1d1a27fb29bfc6ab772faf1f8d0441
Ross Alexander <rossaalexander@gmail.com>
2022-07-05 14:38:40 +0200

    Refactor to use Dispatchable trait

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\ReconcileAccount;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $user = App\Models\User::first();
 
-    dispatch(new \App\Jobs\ReconcileAccount($user));
-
+    ReconcileAccount::dispatch($user);
     return 'Finished';
-
 });
